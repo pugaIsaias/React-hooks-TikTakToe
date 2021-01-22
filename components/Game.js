396d8d2df13cf68ee984c874676b8d95c2fa0aea
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-01-22 00:00:42 -0600

    add Game is working

--- components/Game.js
+++ components/Game.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { calculateWinner } from "../helpers";
 import Board from "./Board";
 
+const style = {
+  width: "200px",
+  margin: "20px auto",
+};
+
 const Game = () => {
   const { board, setBoard } = useState(Array(9).fill(null));
   const [xIsNext, setXisNext] = useState(true);
@@ -19,8 +24,25 @@ const Game = () => {
 
   const jumpTo = () => {};
 
-  const renderMoves = () => {};
+  const renderMoves = () => {
+    return (
+      <button onClick={() => setBoard(Array(9).fill(null))} Start Game></button>
+    );
+  };
 
-  return <Board squares={board} onClick={handleClick} />;
+  return (
+    <>
+      {" "}
+      <Board squares={board} onClick={handleClick} />{" "}
+      <div style={style}>
+        <p>
+          {winner
+            ? "Winner: " + winner
+            : "Next Player: " + (xIsNext ? "X" : "O")}
+        </p>
+        {renderMoves()}
+      </div>{" "}
+    </>
+  );
 };
 export default Game;
