3b325d883f3edb1f10dfbcbc8868fc0e207b7764
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2021-01-21 23:48:44 -0600

    add useState Game

--- components/Game.js
+++ components/Game.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { calculateWinner } from "../helpers";
 import Board from "./Board";
 
 const Game = () => {
+  const { board, setBoard } = useState(Array(9).fill(null));
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
+
   const handleClick = () => {};
 
   const jumpTo = () => {};
 
   const renderMoves = () => {};
 
-  return <Board onClick={handleClick} />;
+  return <Board squares={board} onClick={handleClick} />;
 };
 export default Game;
